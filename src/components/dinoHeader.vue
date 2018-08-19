e4cfbc9e9f01c09e6f171d97c40f816ca4e5e513
Cory Pease <corypease@Corys-MacBook-Pro.local>
2018-08-18 19:02:52 -0600

    initial push

---
+++ src/components/dinoHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+      <img src="img"/>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'dinoHeader',
+  data () {
+    return {
+      img: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
